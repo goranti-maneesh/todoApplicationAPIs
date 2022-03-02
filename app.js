6d302abacb93ee88ea193d91be22d8af6c28f183
goranti-maneesh <gorantimaneeshreddy@gmail.com>
2022-03-02 17:28:14 +0000

    Completed API 1

--- app.js
+++ app.js
@@ -23,8 +23,23 @@ const initializeDBAndServer = async () => {
 
 initializeDBAndServer();
 
+// app.get("/todos/", async (request, response) => {
+//   const setQuery = `SELECT * FROM todo`;
+//   const responseBody = await dataBase.all(setQuery);
+//   response.send(responseBody);
+// });
+
 app.get("/todos/", async (request, response) => {
-  const setQuery = `SELECT * FROM todo`;
+  let { priority = "", status = "", search_q = "" } = request.query;
+  priority =
+    priority === ""
+      ? `priority IN ("LOW", "MEDIUM", "HIGH")`
+      : `priority = '${priority}'`;
+  status =
+    status === ""
+      ? `status IN ("TO DO", "IN PROGRESS", "DONE")`
+      : `status = '${status}'`;
+  const setQuery = `SELECT * FROM todo WHERE ${priority} AND ${status} AND todo LIKE '%${search_q}%'`;
   const responseBody = await dataBase.all(setQuery);
   response.send(responseBody);
 });
